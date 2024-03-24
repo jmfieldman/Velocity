cbf11dd0156ad53ddc7fc7e1404d15b52b2daed7
Jason Fieldman <jason@fieldman.org>
2024-03-24 07:20:59 -0700

    fix some string parsing issues

--- Sources/DependencyMagnetLib/Extensions/String+Extensions.swift
+++ Sources/DependencyMagnetLib/Extensions/String+Extensions.swift
@@ -90,7 +90,13 @@ public extension String {
 
 extension String {
   func dependencyUrlMatch(other: String) -> Bool {
-    lowercased().hasPrefix(other.lowercased()) || other.lowercased().hasPrefix(lowercased())
+    let selfLower = lowercased()
+    let otherLower = other.lowercased()
+
+    if selfLower == otherLower { return true }
+    if otherLower == "\(selfLower).git" { return true }
+    if selfLower == "\(otherLower).git" { return true }
+    return false
   }
 }
 
@@ -100,6 +106,7 @@ extension String {
   ) -> String? {
     let lowercaseHaystack = lowercased()
     return needleMap.keys
+      .sorted(by: { $0.count > $1.count })
       .first { lowercaseHaystack.contains($0.lowercased()) }
       .flatMap { needleMap[$0] }
   }
@@ -116,7 +123,9 @@ extension String {
     let matches = packageRegex.matches(in: linifiedString, range: NSRange(linifiedString.startIndex..., in: linifiedString))
     for match in matches.reversed() {
       let matchedString = String(linifiedString[Range(match.range, in: linifiedString)!])
-      let innerParens = matchedString.contains("(\"")
+      let innerParens = matchedString
+        .replacingOccurrences(of: "package(", with: "")
+        .contains("(")
 
       let nameMatches = nameRegex.matches(in: matchedString, range: NSRange(matchedString.startIndex..., in: matchedString))
       var name = ""
