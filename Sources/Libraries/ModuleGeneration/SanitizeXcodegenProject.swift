6e77430fd918806ff7897b5179c36706dd4ef536
Jason Fieldman <jason@fieldman.org>
2025-08-12 06:11:40 -0700

    finishing up sanitizer

--- Sources/Libraries/ModuleGeneration/SanitizeXcodegenProject.swift
+++ Sources/Libraries/ModuleGeneration/SanitizeXcodegenProject.swift
@@ -89,13 +89,6 @@ public enum SanitizeXcodegenProject {
             try throwError(.fileError, "modules yml at [\(options.modulesYmlPath)] does not contain a top-level 'targets' key")
         }
 
-        // Collect all matches to remove from the build.db file
-
-        var dbMatchesToRemove: [String] = [
-            "\(appName).app",
-            "\(appName).swiftmodule",
-        ]
-
         // Determine derived data directory
 
         let derivedDataPath = "\(kDerivedDataLibraryPath)/\(projectName)-\(hashStringForPath(options.xcodeprojPath.prependingCurrentDirectory()))/Build"
@@ -108,39 +101,88 @@ public enum SanitizeXcodegenProject {
 
         try FileManager.default.createDirectory(atPath: moduleShaDirectory, withIntermediateDirectories: true, attributes: nil)
 
-        // Step -- Remove top-level application product
-
-        try removeApp(
-            derivedDataPath: derivedDataPath,
-            configuration: options.xcodeConfiguration,
-            appName: options.appName ?? projectName
-        )
-
         // Step -- Process modules
 
-        try processModules(targets: targets, dbMatchesToRemove: &dbMatchesToRemove)
+        let dirtyModules = try processModules(
+            targets: targets,
+            moduleShaDirectory: moduleShaDirectory
+        )
 
-        // Final Step -- Clear all DB matches
+        // Names of dirty items
+
+        let dirtyItemNames: Set<String> = Set(
+            [appName] + dirtyModules.keys + dirtyModules.keys.map { $0.replacingOccurrences(of: "-", with: "_") }
+        )
+
+        // Step -- Remove any reference to dirty files
+
+        try dirtyItemNames.forEach {
+            try removeTargetCache(derivedDataPath: derivedDataPath, matching: "'\($0)\\.'")
+        }
+
+        // Step -- Remove any DerivedData files for dirty names
+
+        let pathsToDelete = FileManager.default.enumerateMap(
+            path: derivedDataPath,
+            options: [.skipsHiddenFiles]
+        ) { path, enumerator in
+            let fileRoot = path.lastPathComponent.components(separatedBy: ".").first ?? ""
+            if dirtyItemNames.contains(fileRoot) {
+                enumerator?.skipDescendants()
+                return path
+            }
+            return nil
+        }
+
+        pathsToDelete?.forEach {
+            try? safeDelete(path: $0, prefix: derivedDataPath)
+        }
+
+        // Step -- Clear all DB matches
 
         let dbPath = "\(derivedDataPath)/Intermediates.noindex/XCBuildData/build.db"
         if FileManager.default.fileExists(atPath: dbPath) {
             let dbLink = try Connection(dbPath)
             try dbLink.transaction {
-                try dbMatchesToRemove.forEach {
-                    try dbLink.run("DELETE FROM rule_results WHERE key_id IN (SELECT id FROM key_names WHERE key LIKE '%\($0)%')")
-                    vprint(.debug, "DB Query: DELETE FROM rule_results WHERE key_id IN (SELECT id FROM key_names WHERE key LIKE '%\($0)%')")
+                try dirtyItemNames.forEach {
+                    try dbLink.run("DELETE FROM rule_results WHERE key_id IN (SELECT id FROM key_names WHERE key LIKE '%\($0).%')")
+                    vprint(.debug, "DB Query: DELETE FROM rule_results WHERE key_id IN (SELECT id FROM key_names WHERE key LIKE '%\($0).%')")
                 }
             }
         }
-    }
-}
 
-private func removeApp(derivedDataPath: String, configuration: String, appName: String) throws {
-    try safeDelete(path: "\(derivedDataPath)/Products/\(configuration)-iphoneos/\(appName).app", prefix: derivedDataPath)
-    try safeDelete(path: "\(derivedDataPath)/Products/\(configuration)-iphoneos/\(appName).swiftmodule", prefix: derivedDataPath)
-    try safeDelete(path: "\(derivedDataPath)/Products/\(configuration)-iphonesimulator/\(appName).app", prefix: derivedDataPath)
-    try safeDelete(path: "\(derivedDataPath)/Products/\(configuration)-iphonesimulator/\(appName).swiftmodule", prefix: derivedDataPath)
-    try removeTargetCache(derivedDataPath: derivedDataPath, matching: "\(appName).app")
+        // Step -- Write new module hash files
+
+        try dirtyModules.forEach { moduleName, moduleSha in
+            let shaPath = "\(moduleShaDirectory)/\(moduleName)"
+            try moduleSha.write(toFile: shaPath, atomically: true, encoding: .utf8)
+        }
+
+        // Step -- Update project files' timestamps to match corresponding sha file
+
+        if projectYmlPathArray.count > 0 {
+            var finalHash = ""
+            try projectYmlPathArray.sorted().forEach { projectYmlPath in
+                let projectYmlString = try String(contentsOfFile: projectYmlPath)
+                finalHash += projectYmlString.shaHash()
+            }
+            finalHash = finalHash.shaHash()
+
+            let hashPath = "\(workDirectoryBase)/_project_hash.txt"
+            let onDiskHash = try? String(contentsOfFile: hashPath, encoding: .utf8)
+            if onDiskHash == finalHash {
+                vprint(.verbose, "Project file hash unchanged -- reset timestamps")
+                _ = Process.shellOutput("find -s \(options.xcodeprojPath) -type f -exec touch -r \(hashPath) \"{}\" \\;")
+            } else {
+                if onDiskHash == nil {
+                    vprint(.verbose, "Project file hash created")
+                } else {
+                    vprint(.verbose, "Project file hash changed")
+                }
+                try finalHash.write(toFile: hashPath, atomically: true, encoding: .utf8)
+            }
+        }
+    }
 }
 
 private func removeTargetCache(derivedDataPath: String, matching: String) throws {
@@ -165,8 +207,70 @@ private func safeDelete(path: String, prefix: String) throws {
     }
 }
 
-private func processModules(targets: [String: [String: Any]], dbMatchesToRemove: inout [String]) throws {
-    print("targets: \(targets.keys)")
+/// Returns a dictionary of module names to new hashes (only modules that need wiping)
+private func processModules(
+    targets: [String: [String: Any]],
+    moduleShaDirectory: String
+) throws -> [String: String] {
+    var result: [String: String] = [:]
+    let targetKeys = targets.keys.sorted()
+    for targetName in targetKeys {
+        guard let target = targets[targetName] else { continue }
+        guard let targetType = target["type"] as? String else { continue }
+        guard targetType.hasPrefix("framework") || targetType.hasPrefix("library") else { continue }
+        guard let sources = target["sources"] else {
+            vprint(.debug, "Module [\(targetName)] lacks a 'sources' specification")
+            continue
+        }
+
+        guard let sourcesArray = sources as? [NSDictionary] else {
+            vprint(.debug, "Module [\(targetName)] 'sources' is not an array")
+            continue
+        }
+
+        var docHash = ""
+
+        for source in sourcesArray {
+            guard let sourcePath = source["path"] as? String else {
+                continue
+            }
+
+            guard let sourceHash = directoryContentsHash(relativePath: sourcePath) else {
+                continue
+            }
+
+            docHash += sourceHash
+        }
+
+        guard docHash.count > 0 else {
+            vprint(.verbose, "Module [\(targetName)] has no parsable source files")
+            continue
+        }
+
+        let ymlHash = (target as NSDictionary).deterministicString().shaHash()
+        let finalHash = "\(ymlHash)-\(docHash)".shaHash()
+
+        let shaPath = "\(moduleShaDirectory)/\(targetName)"
+        let shaContents = try? String(contentsOfFile: shaPath, encoding: .utf8)
+        var needsWipe = true
+
+        if let shaContents {
+            if shaContents == finalHash {
+                needsWipe = false
+                vprint(.debug, "Module [\(targetName)] has matching hash")
+            } else {
+                vprint(.verbose, "Module [\(targetName)] has hash mismatch")
+            }
+        } else {
+            vprint(.verbose, "Module [\(targetName)] has no existing hash; creating")
+        }
+
+        if needsWipe {
+            result[targetName] = finalHash
+        }
+    }
+
+    return result
 }
 
 private func hashStringForPath(_ path: String) -> String {
@@ -226,4 +330,12 @@ private func hashStringForPath(_ path: String) -> String {
 
     // Create a new string from the 'resultStr' char array and return
     return String(bytes: resultStr, encoding: .utf8) ?? ""
+}
+
+func directoryContentsHash(relativePath: String) -> String? {
+    FileManager.default.enumerateMap(
+        path: relativePath,
+        options: [.skipsHiddenFiles],
+        itemHandler: { path, _ in path }
+    )?.joined(separator: "|").shaHash()
 }
